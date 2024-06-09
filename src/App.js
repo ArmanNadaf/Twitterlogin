6c2658a312ab7d5b600bd1a64874bda5cf51b9e1
Arman nadaf <armannadaf05@gmail.com>
2024-06-09 08:48:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,33 +1,30 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-   <div className='logo-box'>
-    <img src='./twitter.png' alt='logo' className='logo' />
-    <h2>Sign to twitter</h2>
-    <button >
-    <img src='./google.png' alt='logo' />
-    Sign in with Google
-    </button>
-    <button >
-    <img src='./Apple-Logo.png' alt='logo' />
-    Sign in with Google
-    </button>
-
-    <hr></hr>
-    <span>Or</span>
-    <form>
-      <input type='text' placeholder='Phone email or username'/>
-      <button>Next</button>
-
-    </form>
-    <button>Forget Password</button>
-    <p>Don't Have an Account ?<a>Sign Up</a></p>
-
-
-   </div>
+      <div className="logo-box">
+        <img src="./twitter.png" alt="logo" className="logo" />
+        <h2>Sign in to Twitter</h2>
+        <button>
+          <img src="./google.png" alt="Google logo" />
+          Sign in with Google
+        </button>
+        <button>
+          <img src="./Apple-Logo.png" alt="Apple logo" />
+          Sign in with Apple
+        </button>
+        <hr />
+        <span>Or</span>
+        <form>
+          <input type="text" placeholder="Phone, email, or username" />
+          <button>Next</button>
+        </form>
+        <button>Forgot Password</button>
+        <p>
+          Don't Have an Account? <a href="/signup">Sign Up</a>
+        </p>
+      </div>
     </div>
   );
 }
